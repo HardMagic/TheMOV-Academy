964e66398c592c784b90a4d09279c63f2e389502
Matt Hackney <matt@themov.org>
2014-12-08 19:03:17 +0000

    1.8.2 Update

--- single-wplms-event.php
+++ single-wplms-event.php
@@ -139,6 +139,7 @@ google.maps.event.addDomListener(window, "load", initialize);
                      else {  
                          
                         $product=get_post_meta(get_the_ID(),'vibe_product',true);
+                        print_r($product);
                         setup_postdata( $product ); //  "posts" page is now current post for most template tags        
                         the_content();
                         wp_reset_postdata(); // So everything below functions as normal
