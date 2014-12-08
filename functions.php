bd2c9c813c30968a6811bfa77e15bbdcff6e8a70
Matt Hackney <matt@themov.org>
2014-12-08 17:03:45 +0000

    New Event Layout

--- functions.php
+++ functions.php
@@ -48,4 +48,14 @@ add_filter('dynamic_sidebar_params','widget_first_last_classes');
 
 show_admin_bar(false); 
 
+function event_widget_mov() {
+    register_sidebar( array(
+        'name' => __( 'Event Sidebar', 'event_widget_mov' ),
+        'id' => 'event_widget_mov',
+        'description' => __( 'Widgets in this area will be shown on all events.', 'event_widget_mov' ),
+        'before_title' => '<h1>',
+        'after_title' => '</h1>',
+    ) );
+}
+add_action( 'widgets_init', 'event_widget_mov' );
 ?>
